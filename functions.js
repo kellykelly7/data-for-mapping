f9029c3282a2ecd05d885a38788a6b5770c46374
kellykelly7 <kellytruong519@gmail.com>
2025-12-02 09:33:15 -0500

    changed document folder to git repository
    all of the previously worked on files, just couldn't upload the previous folder as git repository so i had to make a new one and then copy the files over

---
+++ functions.js
@@ -0,0 +1,31 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoia2VsbHlrZWxseTciLCJhIjoiY202aWNjdDE5MDcwbTJrcHppYWw5ZjJzcCJ9.pry2p-gu8qXteiF0TWa4dw'
+
+const map = new mapboxgl.Map({
+    container: 'basemap',
+    projection: 'mercator',
+    style: 'mapbox://styles/kellykelly7/cmiexrzr9005n01qteivxh01o',
+    center: [-79.357577, 43.721446],
+    zoom: 9.75
+})
+
+// Add navigation and fullscreen controls
+map.addControl(new mapboxgl.NavigationControl());
+map.addControl(new mapboxgl.FullscreenControl());
+
+map.on('load', () => {
+    map.addSource('proj-data', {
+        type: 'geojson', 
+        data: 'USI_projects.geojson'
+    })
+
+    map.addLayer({
+        id: 'proj-points',
+        type: 'circle',
+        source: 'proj-data',
+        paint: {
+            'circle-radius': 3,
+            'circle-color': '#FFFF00',
+        }
+    })
+});
+
